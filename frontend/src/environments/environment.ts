d6a580268c40a383c7d550d4aef6e59900601fbe
Yasmin Kruger <mimi_kruger@hotmail.com>
2024-11-29 17:53:54 -0300

    correção de endpoint para pegar localmente

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://api-dev.example.com', // URL da API de desenvolvimento
+  apiUrl: 'https://localhost:44370/api', // URL da API de desenvolvimento
 };
