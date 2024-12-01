cf2413f08f26bd6f44f0a1c162203d4b5bdad0b4
Min Kruger <mimi_kruger@hotmail.com>
2024-12-01 10:45:40 -0300

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44370/api', // URL da API de desenvolvimento
+  apiUrl: 'https://localhost:5000/api', // URL da API de desenvolvimento
 };
